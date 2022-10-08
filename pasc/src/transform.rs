43d9125bb540f24196ecd2377dfee197a20f7a0c
Andrew Merenbach <andrew@merenbach.com>
2022-10-08 00:07:22 -0700

    Add tests and comments for PASC transforms

--- pasc/src/transform.rs
+++ pasc/src/transform.rs
@@ -1,27 +1,100 @@
 use masc::transform as masc_transform;
-use num::Integer;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // TODO: add more tests!!!
+    #[test]
+    fn dummy_works() {
+        let rows = &[
+            (vec![], vec![], 0),
+            (vec![], vec![], 1),
+            (vec![1], vec![1], 0),
+            (vec![1], vec![1], 1),
+            (vec![1, 2], vec![1, 2], 0),
+            (vec![1, 2], vec![1, 2], 1),
+            (vec![1, 2], vec![1, 2], 2),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 0),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 1),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 2),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 3),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 4),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 5),
+        ];
+        for row in rows {
+            println!("{:?}", row);
+            assert_eq!(row.0, dummy(&row.1, row.2));
+        }
+    }
 
-    // #[test]
-    // fn dummy_works() {
-    //     let rows = &[
-    //         vec![],
-    //         vec![1],
-    //         vec![1, 2],
-    //         vec![1, 2, 3],
-    //         vec![1, 2, 3, 4],
-    //         vec![1, 2, 3, 4, 5],
-    //     ];
-    //     for row in rows {
-    //         println!("{:?}", row);
-    //         assert_eq!(row, &dummy(&row));
-    //     }
-    // }
+    #[test]
+    fn vigenere_works() {
+        let rows = &[
+            (vec![], vec![], 0),
+            (vec![], vec![], 1),
+            (vec![1], vec![1], 0),
+            (vec![1], vec![1], 1),
+            (vec![1, 2], vec![1, 2], 0),
+            (vec![2, 1], vec![1, 2], 1),
+            (vec![1, 2], vec![1, 2], 2),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 0),
+            (vec![1, 2, 3, 4, 0], vec![0, 1, 2, 3, 4], 1),
+            (vec![2, 3, 4, 0, 1], vec![0, 1, 2, 3, 4], 2),
+            (vec![3, 4, 0, 1, 2], vec![0, 1, 2, 3, 4], 3),
+            (vec![4, 0, 1, 2, 3], vec![0, 1, 2, 3, 4], 4),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 5),
+        ];
+        for row in rows {
+            println!("{:?}", row);
+            assert_eq!(row.0, vigenere(&row.1, row.2));
+        }
+    }
+
+    #[test]
+    fn beaufort_works() {
+        let rows = &[
+            (vec![], vec![], 0),
+            (vec![], vec![], 1),
+            (vec![1], vec![1], 0),
+            (vec![1], vec![1], 1),
+            (vec![1, 2], vec![1, 2], 0),
+            (vec![2, 1], vec![1, 2], 1),
+            (vec![1, 2], vec![1, 2], 2),
+            (vec![0, 4, 3, 2, 1], vec![0, 1, 2, 3, 4], 0),
+            (vec![1, 0, 4, 3, 2], vec![0, 1, 2, 3, 4], 1),
+            (vec![2, 1, 0, 4, 3], vec![0, 1, 2, 3, 4], 2),
+            (vec![3, 2, 1, 0, 4], vec![0, 1, 2, 3, 4], 3),
+            (vec![4, 3, 2, 1, 0], vec![0, 1, 2, 3, 4], 4),
+            (vec![0, 4, 3, 2, 1], vec![0, 1, 2, 3, 4], 5),
+        ];
+        for row in rows {
+            println!("{:?}", row);
+            assert_eq!(row.0, beaufort(&row.1, row.2));
+        }
+    }
+
+    #[test]
+    fn variant_beaufort_works() {
+        let rows = &[
+            (vec![], vec![], 0),
+            (vec![], vec![], 1),
+            (vec![1], vec![1], 0),
+            (vec![1], vec![1], 1),
+            (vec![1, 2], vec![1, 2], 0),
+            (vec![2, 1], vec![1, 2], 1),
+            (vec![1, 2], vec![1, 2], 2),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 0),
+            (vec![4, 0, 1, 2, 3], vec![0, 1, 2, 3, 4], 1),
+            (vec![3, 4, 0, 1, 2], vec![0, 1, 2, 3, 4], 2),
+            (vec![2, 3, 4, 0, 1], vec![0, 1, 2, 3, 4], 3),
+            (vec![1, 2, 3, 4, 0], vec![0, 1, 2, 3, 4], 4),
+            (vec![0, 1, 2, 3, 4], vec![0, 1, 2, 3, 4], 5),
+        ];
+        for row in rows {
+            println!("{:?}", row);
+            assert_eq!(row.0, variant_beaufort(&row.1, row.2));
+        }
+    }
 
     #[test]
     fn owrap_works() {
@@ -99,27 +172,58 @@ fn owrap<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
 
 // Perform a dummy (no-op) transform on an array slice and return as a vector.
 pub fn dummy<T: Copy>(xs: &[T], _: usize) -> Vec<T> {
-    masc_transform::dummy(&xs)
+    if xs.is_empty() {
+        xs.into()
+    } else {
+        masc_transform::dummy(&xs)
+    }
 }
 
-// Perform a Vigenere transform on an array slice and return as a vector.
+/// Vigenere transform on an array slice, returned as a vector.
+/// This is equivalent to a standard Caesar cipher, shifted by the row index.
+/// Encryption: CT_grid = PT_axis + K_axis
+/// Decryption: PT_axis = CT_grid - K_axis
 pub fn vigenere<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
-    masc_transform::caesar(&xs, i)
+    if xs.is_empty() {
+        xs.into()
+    } else {
+        masc_transform::caesar(&xs, i)
+    }
 }
 
-// Perform a Beaufort transform on an array slice and return as a vector.
+/// Beaufort transform on an array slice, returned as a vector.
+/// This is equivalent to a Vigenere with base ciphertext alphabet and key alphabet reversed.
+/// Encryption: CT_axis = K_grid - PT_axis
+/// Decryption: PT_axis = K_grid - CT_axis
 pub fn beaufort<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
-    masc_transform::affine(&xs, xs.len() - 1, i)
+    if xs.is_empty() {
+        xs.into()
+    } else {
+        masc_transform::affine(&xs, xs.len() - 1, i)
+    }
 }
 
-// // Perform an Atbash transform on an array slice and return as a vector.
+/// Atbash transform on an array slice, returned as a vector.
+/// This is equivalent to a Vigenere with plaintext alphabet, base ciphertext alphabet, and key alphabet reversed.
 // pub fn weird_beaufort<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
-//     masc_transform::affine(&xs, xs.len() - 1, xs.len() - i)
+//     if xs.is_empty() {
+//         xs.into()
+//     } else {
+//         masc_transform::affine(&xs, xs.len() - 1, xs.len() - i)
+//     }
 // }
 
-// Perform a variant Beaufort transform on an array slice and return as a vector.
+/// Variant Beaufort transform on an array slice, returned as a vector.
+/// This is equivalent to a Vigenere with plaintext alphabet and base ciphertext alphabet reversed.
+/// The Variant Beaufort is simply the Vigenere with encipherment and decipherment reversed.
+/// Encryption: CT_axis = PT_grid - K_axis
+/// Decryption: PT_grid = CT_axis + K_axis
 pub fn variant_beaufort<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
-    masc_transform::caesar(&xs, xs.len() - i)
+    if xs.is_empty() {
+        xs.into()
+    } else {
+        masc_transform::caesar(&xs, xs.len() - i)
+    }
 }
 
 // Perform a Della Porta transform on an array slice and return as a vector.
@@ -127,7 +231,11 @@ pub fn della_porta<T: Copy>(xs: &[T], i: usize) -> Vec<T> {
     // TODO: should we enforce an even-length requirement here?
     // TODO: this is an inefficient repeated call to to_vec() under the hood
 
-    let midpoint: usize = xs.len() / 2;
-    let ys = wrap(xs, midpoint);
-    owrap(&ys, i)
+    if xs.is_empty() {
+        xs.into()
+    } else {
+        let midpoint: usize = xs.len() / 2;
+        let ys = wrap(xs, midpoint);
+        owrap(&ys, i)
+    }
 }
