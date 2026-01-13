cd19500f3c49ca12a8bc540d032afaf26c21a49f
Andrew Merenbach <andrew@merenbach.com>
2026-01-12 17:36:36 -0800

    Upgrade edition for cryptosystem package

--- cryptosystem/src/atbash.rs
+++ cryptosystem/src/atbash.rs
@@ -1,7 +1,5 @@
-use crate::simple;
 use cipher::Cipher;
-use masc::transform;
-use masc::Atom;
+use masc::{Atom, SubstitutionCipherBuilder, transform};
 
 #[cfg(test)]
 mod tests {
@@ -66,5 +64,10 @@ mod tests {
 /// Make a monoalphabetic substitution cipher.
 pub fn make<T: Atom>(pt_alphabet: &[T], strict: bool) -> impl Cipher<T, T> {
     let ct_alphabet = transform::atbash(pt_alphabet);
-    simple::make(pt_alphabet, &ct_alphabet, strict)
+    SubstitutionCipherBuilder::default()
+        .pt_alphabet(pt_alphabet)
+        .ct_alphabet(ct_alphabet)
+        .strict(strict)
+        .build()
+        .unwrap()
 }
